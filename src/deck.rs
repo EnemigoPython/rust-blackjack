d8e9939e5145c60bebf1ca04a93dc7d711c273ce
Basil Eagle <basileagle@gmail.com>
2022-05-29 14:29:21 +0100

    deck shuffle and deal

--- src/deck.rs
+++ src/deck.rs
@@ -1,4 +1,6 @@
 use std::fmt;
+use rand::thread_rng;
+use rand::seq::SliceRandom;
 
 #[derive(Debug, PartialEq)]
 enum Value {
@@ -19,6 +21,7 @@ impl Value {
     }
 }
 
+#[derive(Debug, PartialEq)]
 struct Card {
     value: Value,
     name: String,
@@ -56,9 +59,33 @@ impl Deck {
                     name: String::from(*face_card),
                 });
             }
+            for num in 0..9 {
+                let _val = 10 - num;
+                deck.0.push(Card {
+                    value: Value::match_enum(suite, _val),
+                    name: format!("{_val}"),
+                });
+            }
         }
 
         deck
+    }
+
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.0.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, n: usize) -> Vec<Card> {
+        let mut _vec = Vec::new();
+        for _ in 0..n {
+            match self.0.pop() {
+                Some(i) => _vec.push(i),
+                None => panic!("Program tried to deal from an empty Deck"),
+            }
+        }
+
+        _vec
     }
 }
 
@@ -70,7 +97,7 @@ pub mod tests {
     pub fn create_card() {
         let first_card = Card::new(Value::Spades(5), "5");
         assert_eq!(first_card.value, Value::Spades(5));
-        assert_eq!(format!("{first_card}"), "5 of Spades");
+        assert_eq!(first_card.name, "5");
         let second_card = Card::new(Value::Spades(10), "Ace");
         assert_eq!(format!("{second_card}"), "Ace of Spades");
         assert_ne!(first_card.value, second_card.value);
@@ -78,5 +105,34 @@ pub mod tests {
 
     pub fn create_deck() {
         let deck = Deck::new();
+        assert_eq!(deck.0[0].name, "Ace");
+        assert_eq!(format!("{}", deck.0[0]), "Ace of Spades");
+        assert_eq!(deck.0.len(), 52);
+        assert_eq!(deck.0[4].value, Value::Spades(10));
+    }
+
+    pub fn shuffle_deck() {
+        let mut deck = Deck::new();
+        deck.shuffle();
+        let iter = deck.0.iter()
+            .take(13)
+            .fold(true, |acc, curr| {
+                match curr.value {
+                    Value::Spades(_) => if acc { true } else { false }
+                    _ => false
+                }
+            });
+        assert_ne!(iter, true);
+    }
+
+    pub fn deal_from_deck() {
+        let mut deck = Deck::new();
+        let dealt_cards = deck.deal(3);
+        assert_eq!(dealt_cards, vec![
+            Card { value: Value::Diamonds(2), name: String::from("2") },
+            Card { value: Value::Diamonds(3), name: String::from("3") },
+            Card { value: Value::Diamonds(4), name: String::from("4") },
+        ]);
+        assert_eq!(deck.0.len(), 49);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -13,4 +13,14 @@ mod tests {
     fn create_deck() {
         deck_tests::create_deck();
     }
+
+    #[test]
+    fn shuffle_deck() {
+        deck_tests::shuffle_deck();
+    }
+
+    #[test]
+    fn deal_from_deck() {
+        deck_tests::deal_from_deck();
+    }
 }
